d3f8e5abb8fadddaa0dfca9dec7273836d9fde18
Wesleyvdk <76257783+Wesleyvdk@users.noreply.github.com>
2025-02-24 11:20:38 +0100

    Update blog.js

--- src/services/api/blog.js
+++ src/services/api/blog.js
@@ -1,17 +1,17 @@
 import { client } from '../client'
 
 export const fetchPosts = async () => {
-  const response = await client.get(`${API_URL}/api/posts`)
+  const response = await client.get(`${process.env.API_URL}/api/posts`)
   console.log('response in api/blog/fetchPosts', response)
   return response.data
 }
 
 export const createPost = async (postData) => {
-  const response = await client.post(`${API_URL}/api/post`, postData)
+  const response = await client.post(`${process.env.API_URL}/api/post`, postData)
   return response.data
 }
 
 export const getPostById = async (id) => {
-  const response = await client.get(`${API_URL}/api/post/${id}`)
+  const response = await client.get(`${process.env.API_URL}/api/post/${id}`)
   return response.data
 }
